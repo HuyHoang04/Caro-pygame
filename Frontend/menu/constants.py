150f97abaa4ab551038c93c093e68a1e2993935f
DaronD1709 <110324724+DaronD1709@users.noreply.github.com>
2024-11-05 22:27:18 +0700

    Update Frame of board and cell

--- Frontend/menu/constants.py
+++ Frontend/menu/constants.py
@@ -6,7 +6,7 @@ pygame.init()
 # Thiết lập kích thước bảng và các biến
 N = 18  # Kích thước ma trận ban đầu
 WIN_CONDITION = 5  # Số ký tự liên tiếp để thắng
-CELL_SIZE = 30  # Kích thước mỗi ô vuông
+CELL_SIZE = 35  # Kích thước mỗi ô vuông
 WIDTH = 1300
 HEIGHT = 650
 WHITE = (255, 255, 255)
@@ -14,9 +14,9 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 BLUE = (173, 216, 230)  # Màu nền lưới
 BACKGROUND_COLOR = (240, 248, 255)  # Màu nền màn hình
-LINE_WIDTH = 5
+LINE_WIDTH = 2
 FRAME_COLOR = (0, 128, 128)  # Màu khung bao quanh lưới
-FRAME_WIDTH = 10  # Độ dày của khung
+FRAME_WIDTH = 6  # Độ dày của khung
 
 # Thiết lập vị trí lưới để nằm giữa màn hình
 def update_grid_offset():
